02ece8ba5a35caafc7c11f4f9ab3b813da567103
SyedUzairAhmed-AIA <uzair-as.uzair@aia.com>
2024-10-25 18:21:25 +0800

    Initial Commit

---
+++ SeekAsiaAssesment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeekAsiaAssesment
+//
+//  Created by Syed Uzair - Work on 10/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
