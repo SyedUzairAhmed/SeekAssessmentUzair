44f9b934d156969587483c4b84817b97343ee62b
SyedUzairAhmed-AIA <uzair-as.uzair@aia.com>
2024-10-28 18:16:29 +0800

    added HomeApiCall test cases

--- SeekAsiaAssesmentTests/HomeJobList/HomeApiCallTest.swift
+++ SeekAsiaAssesmentTests/HomeJobList/HomeApiCallTest.swift
@@ -6,40 +6,65 @@
 //
 
 import XCTest
+@testable import SeekAsiaAssesment
 
 final class HomeApiCallTest: XCTestCase {
     
-    var sut: UserLoginApiCall!
+    var sut: HomeApiCall!
     var timeoutApi: TimeInterval = 20
 
     override func setUp() {
-        self.sut = UserLoginApiCall()
+        self.sut = HomeApiCall()
     }
     override func tearDown() {
         self.sut = nil
+        super.tearDown()
     }
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    
+    func test_joblist_success_response() {
+        let expectation = self.expectation(description: "Job_Fetch_Success_Returns_JobPostModel")
+        let limit = 10
+        let page = 1
+        
+        sut.fetchJobs(limit: limit, page: page){ response , error  in
+            XCTAssertNil(error)
+            XCTAssertNotNil(response)
+            XCTAssertFalse(response?.isEmpty ?? true)
+            expectation.fulfill()
         }
+        waitForExpectations(timeout: timeoutApi, handler: nil)
+    }
+    
+    func test_joblist_with_invalid_params() {
+        let expectation = self.expectation(description: "Job_Fetch_Failure_Invalid_Params")
+        let limit = -1
+        let page = 999
+        
+        sut.fetchJobs(limit: limit, page: page){ response , error  in
+            XCTAssertNil(response)
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error as? JobsServiceError, JobsServiceError.unknownError)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: timeoutApi, handler: nil)
+    }
+    
+    
+    func test_joblist_network_error() {
+        let expectation = self.expectation(description: "Login_Failur_Network_Error")
+        let limit = 10
+        let page = 1
+        
+        sut.fetchJobs(limit: limit, page: page){ response , error  in
+            
+            // This one will change according to the server responses in this case
+            XCTAssertNil(response)
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error?.localizedDescription, "Network Error")
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: timeoutApi,handler: nil)
     }
 
 }
